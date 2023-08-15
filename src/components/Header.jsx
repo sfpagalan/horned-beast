fa013b59147112f2b07743b9327964a9324a21e9
Sydney Mae Pagalan <smbfpagalan@gmail.com>
2023-08-15 09:21:01 -0700

    removed unnecessary code in header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,6 @@ import React from "react";
 
 
 class Header extends React.Component {
-    constructor(props) {
-        super(props);
-        console.log(props);
-    }
-
     render() {
         return (
             <header>
