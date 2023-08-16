69030f30bd574cc17cf83eea6a527840e67ff619
Sydney Mae Pagalan <smbfpagalan@gmail.com>
2023-08-15 17:32:11 -0700

    added lab-2 features except the bootstrap part

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,36 +1,60 @@
 import React from 'react';
 import HornedBeast from './HornedBeast';
-import Seraphina from '../assets/Seraphina.png';
-import Aurion from '../assets/Aurion.png';
+import hornedBeastData from '../assets/data.json';
+// import Seraphina from '../assets/Seraphina.png';
+// import Aurion from '../assets/Aurion.png';
+
 
 class Gallery extends React.Component {
-    hornedBeastsData = [
-      {
-        title: 'Seraphina, the Enchanted Horn',
-        imageUrl: Seraphina,
-        description: 'Guardian of the mystical realms. Seraphina bridges the gap between mortals and the land of the Horned Beasts.',
-      },
-      {
-        title: 'Aurion, the Ivory Bullheart',
-        imageUrl: Aurion,
-        description: 'Born from the fusion of Taurus strength and lion courage. A guardian of the wild, Aurion embodies both the grounded nature of earth and the fierce spirit of the lion.',
-      },
-    ];
-
-    render() {
-        return (
-          <div className="gallery">
-            {this.hornedBeastsData.map((beast, index) => (
-              <HornedBeast
-                key={index}
-                title={beast.title}
-                imageUrl={beast.imageUrl}
-                description={beast.description}
-              />
-            ))}
-          </div>
-        );
-    }
+  render() {
+    return (
+      <div className='gallery'>
+        {hornedBeastData.map((beast) => (
+          <HornedBeast
+            key={beast._id}
+            image_url={beast.image_url}
+            title={beast.title}
+            description={beast.description}
+            horns={beast.horns}
+          />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default Gallery;
+
+// class Gallery extends React.Component {
+//     hornedBeastsData = [
+//       {
+//         title: 'Seraphina, the Enchanted Horn',
+//         imageUrl: Seraphina,
+//         description: 'Guardian of the mystical realms. Seraphina bridges the gap between mortals and the land of the Horned Beasts.',
+//       },
+//       {
+//         title: 'Aurion, the Ivory Bullheart',
+//         imageUrl: Aurion,
+//         description: 'Born from the fusion of Taurus strength and lion courage. A guardian of the wild, Aurion embodies both the grounded nature of earth and the fierce spirit of the lion.',
+//       },
+//     ];
+
+//     render() {
+//         return (
+//           <div className="gallery">
+//             {this.hornedBeastsData.map((beast, index) => (
+//               <HornedBeast
+//                 key={index}
+//                 title={beast.title}
+//                 imageUrl={beast.imageUrl}
+//                 description={beast.description}
+//                 horns={beast.horns}
+//                 isFavorite={beast.isFavorite}
+//               />
+//             ))}
+//           </div>
+//         );
+//     }
+// }
+
+
